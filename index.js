e21a73178482b302166fec9b6328f35876e7e54a
farfnd <70105993+farfnd@users.noreply.github.com>
2023-11-02 09:31:37 +0000

    done

--- index.js
+++ index.js
@@ -4,17 +4,25 @@ require('dotenv').config()
 
 var app = express();
 
+var multer = require("multer");
+var upload = multer({ dest: 'uploads/' });
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
